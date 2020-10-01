119049759c8f77fc3ad4b35176056728ff3994f2
Lee YoungCheol <ofetwo@gmail.com>
2020-10-02 00:14:50 +0900

    Update 09_02 isOdd

---
+++ 09_Recursion/02_isOdd.js
@@ -0,0 +1,15 @@
+// 수를 입력받아 홀수인지 여부를 리턴해야 합니다.
+function isOdd(num) {
+  if (num === 1 || num === -1) {
+    return true;
+  } else if (num === 0) {
+    return false;
+  }
+
+  if (num > 1) {
+    return isOdd(num - 2);
+  } else if (nem < -1) {
+    return isOdd(num + 2);
+  }
+
+}
